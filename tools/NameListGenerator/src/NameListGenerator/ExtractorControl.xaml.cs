1590f08c387979f3a4310eb2b9bef8a99bb36340
Spacelord-XaN <spacelord.xan@live.at>
2016-06-14 15:59:06 +0200

    Added NameListGenerator project; sw name_lists WIP

---
+++ tools/NameListGenerator/src/NameListGenerator/ExtractorControl.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace NameListGenerator
+{
+    /// <summary>
+    /// Interaction logic for ExtractorControl.xaml
+    /// </summary>
+    public partial class ExtractorControl : UserControl
+    {
+        private readonly List<string> lines;
+        private readonly List<string> result;
+
+        public ExtractorControl()
+        {
+            InitializeComponent();
+
+            this.lines = new List<string>();
+            this.result = new List<string>();
+        }
+
+        private void ParseInput()
+        {
+            this.lines.Clear();
+            string input = this.textBoxInput.Text;
+            string[] lines = input.Split('\n', '\r');
+
+            this.lines.AddRange(lines.Where(X => !string.IsNullOrEmpty(X)));
+        }
+
+        private string CreateText(IEnumerable<string> Lines)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string line in Lines)
+            {
+                sb.AppendLine(line);
+            }
+            return sb.ToString();
+        }
+
+        private void ButtonExtractClick(object Sender, RoutedEventArgs E)
+        {
+            this.ParseInput();
+
+            string keyword = this.textBoxKeyword.Text;
+            this.result.Clear();
+            this.result.AddRange(this.lines.Where(X => X.ToLower().Contains(keyword.ToLower())));
+            this.textBoxResult.Text = this.CreateText(this.result);
+        }
+
+        private void ButtonCopyResultToClipboardClick(object Sender, RoutedEventArgs E)
+        {
+            Clipboard.SetText(this.CreateText(this.result), TextDataFormat.Text);
+        }
+
+        private void ButtonCopyCorrectedSourceClipboardClick(object Sender, RoutedEventArgs E)
+        {
+            Clipboard.SetText(this.CreateText(this.lines.Where( X => !this.result.Contains(X))), TextDataFormat.Text);
+        }
+    }
+}
---
+++ tools/NameListGenerator/src/NameListGenerator/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace NameListGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly List<char> invalidCharacters;
+        private readonly DispatcherTimer timer;
+        private readonly List<string> names;
+        private string lastContent;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.names = new List<string>();
+            this.lastContent = Clipboard.GetText();
+            this.invalidCharacters = new List<char>();
+            this.invalidCharacters.Add('(');
+            this.invalidCharacters.Add(')');
+            this.invalidCharacters.Add('0');
+            this.invalidCharacters.Add('1');
+            this.invalidCharacters.Add('2');
+            this.invalidCharacters.Add('3');
+            this.invalidCharacters.Add('4');
+            this.invalidCharacters.Add('5');
+            this.invalidCharacters.Add('6');
+            this.invalidCharacters.Add('7');
+            this.invalidCharacters.Add('8');
+            this.invalidCharacters.Add('9');
+
+            this.timer = new DispatcherTimer();
+            this.timer.Interval = TimeSpan.FromMilliseconds(300);
+            this.timer.Tick += Timer_Tick;
+            this.timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            string content = Clipboard.GetText();
+            if (content != this.lastContent)
+            {
+                this.lastContent = content;
+                this.AddText(content);
+            }
+        }
+
+        private void AddText(string Text)
+        {
+            string[] lineTokens = Text.Split('\n', '\r');
+            StringBuilder sb = new StringBuilder();
+            foreach (string line in lineTokens)
+            {
+                string name = line;
+                int first = name.IndexOf('(');
+                first--;
+                if (first >= 0)
+                {
+                    name = name.Substring(0, first);
+                }
+                name = name.Replace("\"", "");
+                name = name.Replace("\t", "");
+                if (this.names.Contains(name))
+                {
+                    continue;
+                }
+                this.names.Add(name);
+                sb.Append(name);
+                sb.Append(" ");
+            }
+
+            this.textBloxNames.Text += sb.ToString();
+        }
+
+        private void Button_Click(object Sender, RoutedEventArgs E)
+        {
+            this.names.Sort();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string name in names)
+            {
+                if (name.Contains(' '))
+                {
+                    sb.AppendFormat("\"{0}\"", name);
+                }
+                else
+                {
+                    sb.Append(name);
+                }
+                sb.Append(' ');
+            }
+
+            Clipboard.SetText(sb.ToString(), TextDataFormat.Text);
+        }
+
+        private void ButtonResetClick(object Sender, RoutedEventArgs E)
+        {
+            this.timer.Stop();
+            Clipboard.Clear();
+            this.lastContent = null;
+            this.names.Clear();
+            this.textBloxNames.Text = "";
+            this.timer.Start();
+        }
+    }
+}
